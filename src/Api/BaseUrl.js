6b12a0ab54d9c8a54a6ed0d09af4995bb12873e3
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-11-09 12:29:14 +0200

    Update BaseUrl.js

--- src/Api/BaseUrl.js
+++ src/Api/BaseUrl.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-const BaseUrl = axios.create({ baseURL: "https://ecommerce-app-flax-iota.vercel.app" });
+const BaseUrl = axios.create({ baseURL: "https://ecommerce-back-end-k901.onrender.com" });
 export default BaseUrl;
