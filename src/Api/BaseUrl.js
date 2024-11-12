f896acc74aab764fb445e92c98ddd250104f007b
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2024-11-12 18:39:36 +0200

    Update BaseUrl.js

--- src/Api/BaseUrl.js
+++ src/Api/BaseUrl.js
@@ -1,3 +1,5 @@
 import axios from "axios";
-const BaseUrl = axios.create({ baseURL: "https://ecommerce-back-end-k901.onrender.com" });
+const BaseUrl = axios.create({
+  baseURL: "https://ecommerce-back-end-production.up.railway.app",
+});
 export default BaseUrl;
